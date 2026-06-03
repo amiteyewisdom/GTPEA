a392b48b2d355932ccfd723e35c94991a65a453b
Wisdom Yaw Amiteye <amiteyewisdom00@gmail.com>
2026-06-03 11:11:54 +0000

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,6 @@
-
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen w-full bg-white">
+    <div className="min-h-screen w-full bg-[#0d121f]">
       {children}
     </div>
   );
